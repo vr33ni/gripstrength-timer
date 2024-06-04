19c649e58d6379b684e902c8b2eac7aea4852737
Verena Lechner <v.m.s.lechner@gmail.com>
2024-06-04 20:47:08 +0200

    Initial Commit

---
+++ HangingTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HangingTimer
+//
+//  Created by Verena Lechner on 04/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HangingTimer/HangingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangingTimerApp.swift
+//  HangingTimer
+//
+//  Created by Verena Lechner on 04/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct HangingTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
